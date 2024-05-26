811fee0bd689c4bcdb052a79a6e14ffada0d904e
Anas Shaikh <shaikhanas246@gmail.com>
2024-05-26 13:02:41 +0530

    classprops in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,9 +4,10 @@ interface IProps {
   variant?: string,
   width?: string,
   btnType?: "button" | "submit" | "reset" | undefined
+  classProps?: string
 }
 
-export default function Button({ title, variant, width, ...rest }: IProps) {
+export default function Button({ title, variant, width, classProps }: IProps) {
 
 
   let btnVariant: string = '';
@@ -29,7 +30,7 @@ export default function Button({ title, variant, width, ...rest }: IProps) {
   }
 
   return (
-    <button {...rest} className={`border-2 shadow-sm px-6 py-2 rounded-lg font-semibold hover:transition-all ${btnVariant} ${getWidth}`}>
+    <button className={`border-2 shadow-sm px-6 py-2 rounded-lg font-semibold hover:transition-all ${btnVariant} ${getWidth} ${classProps}`}>
       {title}
     </button>
   )
