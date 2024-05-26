bb6ce0f4550b452452176d9649935ba7a53f3cf9
Anas Shaikh <shaikhanas246@gmail.com>
2024-05-26 13:03:23 +0530

    new post Ui cleanup

--- src/components/textEditor/TextEditor.tsx
+++ src/components/textEditor/TextEditor.tsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-export default function TextEditor() {
-  const [value, setValue] = useState('');
+interface ITextEditor {
+  value: string,
+  setValue: any
+}
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+
+export default function TextEditor({ value, setValue }: ITextEditor) {
+
+
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+      ['link', 'image'],
+      ['clean']
+    ],
+  }
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
+
+  return <ReactQuill theme="snow" value={value} onChange={setValue} modules={modules} formats={formats} />;
 
 }
