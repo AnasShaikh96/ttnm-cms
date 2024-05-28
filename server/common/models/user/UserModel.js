b576b6492ebc682c8d8bbdf9d21fb2da8baa5fd0
Anas Shaikh <shaikhanas246@gmail.com>
2024-05-28 22:15:11 +0530

    added controllers

---
+++ server/common/models/user/UserModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+const UserModel = new mongoose.Schema({
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
+  },
+})
+
+
+module.exports = {
+  initialize: (mongoose) => {
+    return this.model = mongoose.model('user', UserModel)
+  },
+  create: (user) => {
+    return this.model.create(user)
+  },
+  find: (query) => {
+    return this.model.find(query)
+  },
+} 
