7baf4200dcdb3cfa548b5c3ff6c15f4333d98729
Anas Shaikh <shaikhanas246@gmail.com>
2024-06-01 20:36:46 +0530

    downgraded version of tailwind mui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
