36ee4410281558b5c23e18e518664fc83ea5dd99
Anas Shaikh <shaikhanas246@gmail.com>
2024-06-01 18:45:48 +0530

    revamp login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,22 +1,22 @@
-// import Button from "../../components/Button";
-
-import { Button } from "@material-tailwind/react";
+import { Button, Input } from "@material-tailwind/react";
 
 export default function Login() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-1/3 h-fit border p-10 rounded-lg ">
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
+      <div className="w-[36rem] h-fit border p-10 rounded-lg bg-gray-50 shadow-sm shadow-black-700">
 
-        <h2 className="text-xl text-center mb-4">Login</h2>
+        <h2 className="text-4xl text-center mb-10 font-serif font-bold ">Clone<span className="text-deep-purple-600 underline underline-offset-4">ium</span></h2>
 
-        <label htmlFor="email" className="text-gray-500">Email</label>
-        <input type="email" name="email" className="w-full border p-2 mb-5 rounded-lg" />
+        <div className="mb-7">
+          <Input variant="outlined" color="gray" label="Email" type="email" placeholder="Email" crossOrigin={undefined} />
+        </div>
+        <div className="mb-10">
+          <Input variant="outlined" color="gray" label="Password" type="password" placeholder="Password" crossOrigin={undefined} />
+        </div>
 
-        <label htmlFor="email" className="text-gray-500">Password</label>
-        <input type="password" name="email" className="w-full border p-2 mb-6 rounded-lg" />
-
-        <div className="grid grid-cols-2 gap-x-4">
-          <Button variant="filled"> Sign Up</Button>
+        <div className="grid grid-cols-1 gap-y-4">
+          <Button variant="filled" color="deep-purple">Login</Button>
+          <Button variant="outlined" color="deep-purple">Sign Up</Button>
         </div>
 
       </div>
