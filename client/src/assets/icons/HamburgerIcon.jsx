62602f53ece7a95a6b5ba4cc1315fa41a3a4af61
Anas Shaikh <shaikhanas246@gmail.com>
2024-06-21 20:45:52 +0530

    added icons

---
+++ client/src/assets/icons/HamburgerIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function HamburgerIcon() {
+  return (
+    <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 8L21 12L17 16M3 12H13M3 6H13M3 18H13" /></svg>)
+}
