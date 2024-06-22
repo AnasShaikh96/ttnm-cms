35b657fb8c9ce79d03e04199c0821c6f68c0e203
Anas Shaikh <shaikhanas246@gmail.com>
2024-06-22 12:35:52 +0530

    Posts page refactoring

--- client/src/pages/posts/Posts.tsx
+++ client/src/pages/posts/Posts.tsx
@@ -6,6 +6,7 @@ export default function Posts() {
     { name: 'Status', field: 'status' },
     { name: 'Created At', field: 'created_at' },
     { name: 'Last Updated', field: 'last_updated' },
+    { name: '', field: 'link' },
   ]
 
   const postData = [
@@ -16,7 +17,15 @@ export default function Posts() {
       status: 'active',
       created_at: '12-12-2024',
       last_updated: '12-12-2023',
-    }
+    },
+    {
+      id: 'oiasd-oihasdi-poajd',
+      title: 'Two day in a life',
+      slug_name: '/two-day',
+      status: 'active',
+      created_at: '12-12-2024',
+      last_updated: '12-12-2023',
+    },
   ]
 
 
@@ -66,224 +75,66 @@ export default function Posts() {
                             </div>
                           </th>
                         ))}
-                        <th scope="col" className="px-6 py-3 text-end"></th>
                       </tr>
                     </thead>
 
                     <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
-                      <tr>
-                        {postHeader.map((row: any, rowIndex) =>
 
-                          postData.map((item: any, index) => {
-                            if (row.field === 'title') {
-                              return (
-                                <td className="size-px whitespace-nowrap">
-                                  <div className="ps-6 lg:ps-3 xl:ps-3 pe-6 py-3">
-                                    <div className="flex items-center gap-x-3">
-                                      <div className="grow">
-                                        <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">
-                                          {item[row.field]}
-                                        </span>
-                                      </div>
+                      {postData.map((cell: any) => {
+
+                        const getData = postHeader.map((row: any, rowIndex) => {
+
+
+                          if (row.field === 'title') {
+
+                            return (
+                              <td className="size-px whitespace-nowrap">
+                                <div className="ps-6 lg:ps-3 xl:ps-3 pe-6 py-3">
+                                  <div className="flex items-center gap-x-3">
+                                    <div className="grow">
+                                      <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">{cell[row.field]}</span>
                                     </div>
                                   </div>
-                                </td>
-                              )
-                            } else if (row.field === 'status') {
-                              return (<td className="size-px whitespace-nowrap">
-                                <div className="px-6 py-3">
+                                </div>
+                              </td>)
+
+                          } else if (row.field === 'status') {
+                            return (
+                              <td className="size-px whitespace-nowrap">
+                                <div className="px-3 py-3">
                                   <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
                                     <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                                     </svg>
-                                    {item[row.field]}
+                                    {cell[row.field]}
                                   </span>
                                 </div>
-                              </td>)
-                            } else {
-                              return (
-                                <td className="size-px whitespace-nowrap">
-                                  <div className="px-3 py-3">
-                                    <span className="block text-sm text-gray-500 dark:text-neutral-500">
-                                      {item[row.field]}
-                                    </span>
-                                  </div>
-                                </td>
-                              )
-                            }
+                              </td>
+                            )
+                          } else if (row.field === 'link') {
+                            return (
+                              <td className="size-px whitespace-nowrap">
+                                <div className="px-6 py-1.5">
+                                  <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="/">
+                                    Edit
+                                  </a>
+                                </div>
+                              </td>
+                            )
+                          } else {
 
-                          })
-                        )}
-                      </tr>
+                            return (
+                              <td className="size-px whitespace-nowrap">
+                                <div className="px-3 py-3">
+                                  <span className="block text-sm text-gray-500 dark:text-neutral-500">{cell[row.field]}</span>
+                                </div>
+                              </td>
+                            )
 
-
-                      {/* <tr>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="ps-6 lg:ps-3 xl:ps-3 pe-6 py-3">
-                            <div className="flex items-center gap-x-3">
-
-                              <div className="grow">
-                                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Christina Bersh</span>
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-3 py-3">
-                            <span className="block text-sm text-gray-500 dark:text-neutral-500">Human resources</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
-                              <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                              </svg>
-                              Active
-                            </span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="text-sm text-gray-500 dark:text-neutral-500">28 Dec, 12:12</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="text-sm text-gray-500 dark:text-neutral-500">28 Dec, 12:12</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-1.5">
-                            <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="/">
-                              Edit
-                            </a>
-                          </div>
-                        </td>
-                      </tr> */}
-
-                      {/* <tr>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="ps-6 py-3">
-                            <label htmlFor="hs-at-with-checkboxes-2" className="flex">
-                              <input type="checkbox" className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-at-with-checkboxes-2" />
-                              <span className="sr-only">Checkbox</span>
-                            </label>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
-                            <div className="flex items-center gap-x-3">
-                              <img className="inline-block size-[38px] rounded-full" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" alt="Description" />
-                              <div className="grow">
-                                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">David Harrison</span>
-                                <span className="block text-sm text-gray-500 dark:text-neutral-500">david@site.com</span>
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="h-px w-72 whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Seller</span>
-                            <span className="block text-sm text-gray-500 dark:text-neutral-500">Branding products</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full dark:bg-yellow-500/10 dark:text-yellow-500">
-                              <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                              </svg>
-                              Warning
-                            </span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <div className="flex items-center gap-x-3">
-                              <span className="text-xs text-gray-500 dark:text-neutral-500">3/5</span>
-                              <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700">
-                            </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="text-sm text-gray-500 dark:text-neutral-500">20 Dec, 09:27</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-1.5">
-                            <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="/">
-                              Edit
-                            </a>
-                          </div>
-                        </td>
-                      </tr> */}
-
-
-                      {/* 
-                      <tr>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="ps-6 py-3">
-                            <label htmlFor="hs-at-with-checkboxes-5" className="flex">
-                              <input type="checkbox" className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-at-with-checkboxes-5" />
-                              <span className="sr-only">Checkbox</span>
-                            </label>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
-                            <div className="flex items-center gap-x-3">
-                              <span className="inline-flex items-center justify-center size-[38px] rounded-full bg-white border border-gray-300 dark:bg-neutral-800 dark:border-neutral-700">
-                                <span className="font-medium text-sm text-gray-800 leading-none dark:text-neutral-200">D</span>
-                              </span>
-                              <div className="grow">
-                                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">David Harrison</span>
-                                <span className="block text-sm text-gray-500 dark:text-neutral-500">david@site.com</span>
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="h-px w-72 whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Developer</span>
-                            <span className="block text-sm text-gray-500 dark:text-neutral-500">Mobile app</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-red-100 text-red-800 rounded-full dark:bg-red-500/10 dark:text-red-500">
-                              <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                              </svg>
-                              Danger
-                            </span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <div className="flex items-center gap-x-3">
-                              <span className="text-xs text-gray-500 dark:text-neutral-500">3/5</span>
-                              <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700">
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-3">
-                            <span className="text-sm text-gray-500 dark:text-neutral-500">15 Dec, 14:41</span>
-                          </div>
-                        </td>
-                        <td className="size-px whitespace-nowrap">
-                          <div className="px-6 py-1.5">
-                            <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="/">
-                              Edit
-                            </a>
-                          </div>
-                        </td>
-                      </tr> */}
-
+                          }
+                        })
+                        return <tr>{getData}</tr>
+                      })}
                     </tbody>
                   </table>
 
