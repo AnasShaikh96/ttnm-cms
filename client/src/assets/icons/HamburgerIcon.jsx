80c8ec51871466e668ce0c08908c116d21784817
Anas Shaikh <shaikhanas246@gmail.com>
2024-06-22 13:08:08 +0530

    Removed eslint warnings for icons

--- client/src/assets/icons/HamburgerIcon.jsx
+++ client/src/assets/icons/HamburgerIcon.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 
 export default function HamburgerIcon() {
   return (
-    <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 8L21 12L17 16M3 12H13M3 6H13M3 18H13" /></svg>)
+    <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M17 8L21 12L17 16M3 12H13M3 6H13M3 18H13" /></svg>)
 }
--- client/src/pages/posts/Posts.tsx
+++ client/src/pages/posts/Posts.tsx
@@ -101,7 +101,7 @@ export default function Posts() {
                         </a>
 
                         <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="/">
-                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
                           Add post
                         </a>
                       </div>
@@ -113,7 +113,7 @@ export default function Posts() {
                       <tr>
 
                         {postHeader.map((item) => (
-                          <th scope="col" className="ps-6 lg:ps-3 xl:px-3  py-3 text-start">
+                          <th scope="col" className="ps-6 lg:ps-3 xl:px-3  py-3 text-start" key={item.name}>
                             <div className="flex items-center gap-x-2">
                               <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
                                 {item.name}
@@ -145,7 +145,7 @@ export default function Posts() {
 
                           }
                         })
-                        return <tr>{getData}</tr>
+                        return <tr key={cell.id}>{getData}</tr>
                       })}
                     </tbody>
                   </table>
@@ -160,13 +160,13 @@ export default function Posts() {
                     <div>
                       <div className="inline-flex gap-x-2">
                         <button type="button" className="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6" /></svg>
+                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6" /></svg>
                           Prev
                         </button>
 
                         <button type="button" className="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
                           Next
-                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg>
+                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
                         </button>
                       </div>
                     </div>
