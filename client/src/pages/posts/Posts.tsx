90af2231e286337e1f48d1bc7bc4cfa0cf9c2f13
Anas Shaikh <shaikhanas246@gmail.com>
2024-06-22 19:02:31 +0530

    refactor posts table

--- client/src/pages/posts/Posts.tsx
+++ client/src/pages/posts/Posts.tsx
@@ -1,3 +1,9 @@
+import { NavLink } from "react-router-dom"
+import PlusIcon from "../../assets/icons/PlusIcon"
+import LinkCell from "../../components/Cell/LinkCell"
+import StatusCell from "../../components/Cell/StatusCell"
+import TitleCell from "../../components/Cell/TitleCell"
+
 export default function Posts() {
 
   const postHeader = [
@@ -5,15 +11,7 @@ export default function Posts() {
       name: 'Title',
       field: 'title',
       cellEdit: function (cell: any) {
-        return (<td className="size-px whitespace-nowrap">
-          <div className="ps-6 lg:ps-3 xl:ps-3 pe-6 py-3">
-            <div className="flex items-center gap-x-3">
-              <div className="grow">
-                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">{cell[this.field] ?? ''} </span>
-              </div>
-            </div>
-          </div>
-        </td>)
+        return <TitleCell title={cell[this.field] ?? ''} />
       },
     },
     { name: 'Slug Name', field: 'slug_name' },
@@ -21,18 +19,9 @@ export default function Posts() {
       name: 'Status',
       field: 'status',
       cellEdit: function (cell: any) {
-        return (
-          <td className="size-px whitespace-nowrap">
-            <div className="px-3 py-3">
-              <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
-                <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                </svg>
-                {cell[this.field] ?? ''}
-              </span>
-            </div>
-          </td>
-        )
+        return <StatusCell title={cell[this.field] ?? ''} />
+
+
       }
     },
     { name: 'Created At', field: 'created_at' },
@@ -41,16 +30,7 @@ export default function Posts() {
       name: '',
       field: 'link',
       cellEdit: function (cell: any) {
-        return (
-          <td className="size-px whitespace-nowrap">
-            <div className="px-6 py-1.5">
-              <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href={cell[this.field]}>
-                Edit
-              </a>
-            </div>
-          </td>
-        )
-
+        return <LinkCell link={cell[this.field]} />
       }
     },
   ]
@@ -96,14 +76,11 @@ export default function Posts() {
 
                     <div>
                       <div className="inline-flex gap-x-2">
-                        <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" href="/">
-                          View all
-                        </a>
-
-                        <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="/">
-                          <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+                        <NavLink to="/new-post" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                        >
+                          <PlusIcon />
                           Add post
-                        </a>
+                        </NavLink>
                       </div>
                     </div>
                   </div>
